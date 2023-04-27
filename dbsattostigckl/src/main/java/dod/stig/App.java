9b9b6c1642743d2623843eb5ec71ec4b160fb0d1
garyplattenburg <gary.plattneburg@gmail.com>
2023-04-27 15:24:47 -0400

    Adding basic java project

---
+++ dbsattostigckl/src/main/java/dod/stig/App.java
@@ -0,0 +1,13 @@
+package dod.stig;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
